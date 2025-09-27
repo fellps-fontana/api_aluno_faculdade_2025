48d0ed371ec9eb41fe2c645e8d4e3386cbf956d5
001-FELIPE.FONTANA-Informatica <FELIPE.FONTANA@cooperalfa.coop.br>
2025-09-26 21:33:04 -0300

    Removidas pastas irrelevantes do repositório, mantendo apenas trabalho_final_aluno

--- BackAlunos/src/main/java/com/api/alunos/repositorys/AlunosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.alunos.repositorys;
-
-import com.api.alunos.models.AlunoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AlunosRepository extends JpaRepository<AlunoModel, UUID> {
-}
